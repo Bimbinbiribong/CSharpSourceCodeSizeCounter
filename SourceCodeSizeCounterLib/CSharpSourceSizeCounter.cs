e9d5614df6064a6793b4e14230aa53c0a76a2952
Jan Palášek <jan.palasek@seznam.cz>
2017-06-29 08:58:03 +0200

    Unauthorized access to directories fixed.

--- SourceCodeSizeCounterLib/CSharpSourceSizeCounter.cs
+++ SourceCodeSizeCounterLib/CSharpSourceSizeCounter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -30,14 +31,34 @@ namespace SourceCodeSizeCounterLib
             return Task.Run(() => Count(sourceDirectory));
         }
 
+        /// <summary>
+        /// Counts size of C sharp source files and returns its sum in bytes.
+        /// </summary>
+        /// <returns></returns>
+        public long Count()
+        {
+            return Count(sourceDirectory);
+        }
+
         long Count(DirectoryInfo dir)
         {
             long sourceLengths = 0;
 
-            var subDirectories = dir.GetDirectories(); // get sub-directories
+            DirectoryInfo[] subDirectories = null;
+            try
+            {
+                subDirectories = dir.GetDirectories(); // get sub-directories
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return 0;
+            }
             
             foreach (DirectoryInfo directory in subDirectories)
+            {
                 sourceLengths += Count(directory); // count files in its subdirectories
+            }
+
 
             var files = dir.GetFiles();
 
@@ -45,14 +66,14 @@ namespace SourceCodeSizeCounterLib
             Regex csRegex = new Regex(@"\.cs$");
 
             var filteredFiles = from file in files.AsParallel()
-                where !designerRegex.IsMatch(file.Name) // filters out .Designer.cs files
-                where csRegex.IsMatch(file.Name) // takes .cs files
-                where !file.Name.StartsWith("TemporaryGeneratedFile_") // filters out generated files
-                where file.Name != "AssemblyInfo.cs"
-                select file;
+                                where !designerRegex.IsMatch(file.Name) // filters out .Designer.cs files
+                                where csRegex.IsMatch(file.Name) // takes .cs files
+                                where !file.Name.StartsWith("TemporaryGeneratedFile_") // filters out generated files
+                                where file.Name != "AssemblyInfo.cs"
+                                select file;
 
             sourceLengths += (from file in filteredFiles select file.Length).DefaultIfEmpty().Sum(); // counts Size
-            
+
             return sourceLengths;
         }
     }
