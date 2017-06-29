c3abf28cc7813cd29ad41221487b124026f3ba63
Jan Palášek <jan.palasek@seznam.cz>
2017-06-29 08:59:54 +0200

    Reworked to synchronous call.

--- SourceCodeSizeCounter/Program.cs
+++ SourceCodeSizeCounter/Program.cs
@@ -13,7 +13,7 @@ namespace SourceCodeSizeCounter
         static void Main(string[] args)
         {
             CSharpSourceSizeCounter c = new CSharpSourceSizeCounter(args[0]);
-            long filesCountBytes = c.CountAsync().Result;
+            long filesCountBytes = c.Count();
             Console.WriteLine($"Bytes count: {filesCountBytes}");
             Console.WriteLine($"Kilobytes count: {filesCountBytes / 1024} (truncated)");
         }
